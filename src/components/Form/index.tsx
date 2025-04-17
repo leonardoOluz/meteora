d9c112af247b4b55c3579801f45abccdd828c6d3
Leonardo Luz <leonardoluz10@hotmail.com>
2025-04-17 18:37:25 -0300

    feat: :white_check_mark: Feat: Adicionado handleSubmit no modalProduto.

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -2,9 +2,15 @@ import { FormStyle } from "./styles";
 
 interface IProps {
   children: React.ReactNode;
+  ariaLabel: string;
+  handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
 }
-const Form = ({ children }: IProps) => {
-  return <FormStyle>{children}</FormStyle>;
+const Form = ({ children, ariaLabel, handleSubmit }: IProps) => {
+  return (
+    <FormStyle aria-label={ariaLabel} onSubmit={handleSubmit}>
+      {children}
+    </FormStyle>
+  );
 };
 
 export default Form;
--- src/components/ModalProduto/styles.ts
+++ src/components/ModalProduto/styles.ts
@@ -3,14 +3,14 @@ import styled from "styled-components";
 
 export const DialogModal = styled.dialog`
   position: fixed;
-  z-index: 999;
+  z-index: 1;
   left: 0;
   top: 0;
   bottom: 0;
   width: 100%;
   height: 100%;
   overflow: auto;
-  background-color: rgba(0, 0, 0, 0.05);
+  background-color: rgba(0, 0, 0, 0.5);
   display: flex;
   flex-direction: row;
   justify-content: center;
